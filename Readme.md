fddac14a01f30656913549d84dc4fdd7fbf1a667
ahmed <ahmedseyoufi009@gmail.com>
2024-05-21 11:39:47 +0300

    added task1

--- Readme.md
+++ Readme.md
@@ -1,5 +1,3 @@
 # OOP-practise
 
 here I will upload my projects with oop
-
-## hello
--- task1.cpp
+++ task1.cpp
@@ -1,3 +1,46 @@
-//
-// Created by ahmed on 5/21/2024.
-//
+#include <iostram>
+
+using namespace std;
+
+class date{
+    private:
+       int Day, Month, Year;
+
+       //style 1
+       string S1();
+
+       //style 2
+       string S2();
+
+       //style3
+       string S3();
+
+    public:
+        date(int Day, int Month, int Year);
+        string GetDate(int Style);
+
+};
+
+class time{
+    private:
+        int sec, min, hours;
+        //style 1
+        string S1();
+
+        //style 2
+        string S2();
+
+        //style3
+        string S3();
+
+    public:
+        time(int sec, int min, int hours);
+        string GetTime(int Style);
+        int GetSec();
+        int GetMin();
+        int Gethours();
+};
+
+int main(){
+
+}
