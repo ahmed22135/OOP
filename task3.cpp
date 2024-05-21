bcea53967c73ac95ff9d150f0e4c09de448c3b37
ahmed <ahmedseyoufi009@gmail.com>
2024-05-21 14:01:16 +0300

    updated task3

--- task3.cpp
+++ task3.cpp
@@ -10,7 +10,7 @@ private:
     int quantity;
 public:
     Invoice(string name, int item_num, int price, int quantity);
-    void SetName();
+    void SetName(string name);
     void SetItemNum();
     void SetPrice();
     void SetQuantity();
@@ -22,8 +22,60 @@ public:
     int GetQuantity();
 };
 
-Invoice :: 
+void Invoice :: SetName(string name){
+    this->name = name;
+}
 
+void Invoice :: SetItemNum(int item_num){
+    this->item_num = item_num;
+}
+
+void Invoice :: SetPrice(int price){
+    this->price = price;
+}
+
+void Invoice :: SetQuantity(int quantity){
+    this->quantity = quantity;
+}
+
+string Invoice :: GetName(){
+    return name;
+}
+
+int Invoice :: GetItemNum(){
+    return item_num;
+}
+
+int Invoice :: GetPrice(){
+    return price;
+}
+
+int Invoice :: GetQuantity(){
+    return quantity;
+}
+
+string Invoice :: to_string(){
+    string ret = "";
+    ret += name;
+    ret += ',';
+    ret += to_string(item_num);
+    ret += ',';
+    ret += to_string(price);
+    ret += ',';
+    ret += to_string(quantity);
+    return ret;
+}
+
+int Invoice :: GetTotalPrice(){
+    return quantity * price;
+}
+
+Invoice :: Invoice(string name, int item_num, int price, int quantity){
+    this->name = name;
+    this->item_num = item_num;
+    this->price = price;
+    this->quantity = quantity;
+}
 int main(){
 
     return 0;
