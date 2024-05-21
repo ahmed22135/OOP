c0d7c254d2f29bf4a3dd54dad430d0f69f0d7106
ahmed <ahmedseyoufi009@gmail.com>
2024-05-21 13:32:00 +0300

    added task2

--- task2.cpp
+++ task2.cpp
@@ -1,3 +1,59 @@
-//
-// Created by ahmed on 5/21/2024.
-//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class room{
+private:
+    int length, width;
+    int beds;
+    int desktop, area;
+public:
+    room(int length, int width, int beds, int desktop);
+    void add_bed();
+    void add_desktop();
+    int get_area();
+    int get_num_of_beds();
+    int get_num_of_desktop();
+};
+
+class elevator{
+private:
+    int max_num_of_people = 4;
+public:
+    elevator();
+    int get_max_num_of_people();
+};
+
+class bathroom{
+private:
+    int length, width, area;
+public:
+    bathroom(int length, int width);
+    int get_area();
+};
+
+class apartment{
+private:
+    int length, width, area;
+    vector<room>Room;
+    vector<bathroom>Bath;
+public:
+    apartment(int length, int area, int NumOfRooms, int NumOfBathrooms);
+    int GetNumOfRooms();
+    int GetNumOfBathrooms();
+    int GetArea();
+};
+
+class building{
+private:
+    vector<apartment>Apart;
+    vector<elevtor>elev;
+public:
+    building(int NumOfApartments, int NumOfElevators);
+    int GetNumOfApartments();
+    int GetNumOfElevators();
+};
+
+int main(){
+    return 0;
+}
